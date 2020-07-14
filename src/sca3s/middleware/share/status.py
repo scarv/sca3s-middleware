f7e3e3ee712e64316bdc6046b61ebeb192d4a74c
James Webb <james@ultra-horizon.com>
2020-07-14 21:08:47 +0100

    Ensure Status dictionaries are hex encoded.

--- src/sca3s/middleware/share/status.py
+++ src/sca3s/middleware/share/status.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 SCARV project <info@scarv.org>
 #
-# Use of this source code is restricted per the MIT license, a copy of which 
-# can be found at https://opensource.org/licenses/MIT (or should be included 
+# Use of this source code is restricted per the MIT license, a copy of which
+# can be found at https://opensource.org/licenses/MIT (or should be included
 # as LICENSE.txt within the associated archive or repository).
 
 import enum, re
@@ -31,9 +31,9 @@ class Status(enum.IntEnum):
     def dict(status: enum.IntEnum, data=None) -> dict:
         if data is None:
             return {
-                'status': status
+                'status': status.hex()
             }
-        data['status'] = status
+        data['status'] = status.hex()
         return data
 
     # status values: generic
