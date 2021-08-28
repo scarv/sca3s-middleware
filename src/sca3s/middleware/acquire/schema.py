a7226435a72b423ae43b9a67809660b4ebf564f6
Dan Page <dan@phoo.org>
2021-08-28 19:53:07 +0100

    update schema to reflect upgrade to function kernel

--- src/sca3s/middleware/acquire/schema.py
+++ src/sca3s/middleware/acquire/schema.py
@@ -38,26 +38,56 @@ MANIFEST_REQ = {
     }, 'required' : [] },
     # trace spec. specific fields: kernel = aead     
     'trace_spec_specific_aead'     : { 'type' :  'object', 'default' : {}, 'properties' : {
-      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/k', 'data/usedof_k', 'data/n', 'data/usedof_n', 'data/a', 'data/usedof_a', 'data/m', 'data/usedof_m', 'data/c', 'data/usedof_c' ], 'items' : {
-        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/k', 'data/usedof_k', 'data/n', 'data/usedof_n', 'data/a', 'data/usedof_a', 'data/m', 'data/usedof_m', 'data/c', 'data/usedof_c' ]
+      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/k',  'data/usedof_k', 
+                                                                                   'data/n',  'data/usedof_n', 
+                                                                                   'data/a',  'data/usedof_a', 
+                                                                                   'data/m',  'data/usedof_m', 
+                                                                                   'data/c',  'data/usedof_c'  ], 'items' : {
+        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/k',  'data/usedof_k', 
+                                                                                                                                           'data/n',  'data/usedof_n', 
+                                                                                                                                           'data/a',  'data/usedof_a', 
+                                                                                                                                           'data/m',  'data/usedof_m', 
+                                                                                                                                           'data/c',  'data/usedof_c'  ]
       } }
     } },
     # trace spec. specific fields: kernel = block    
     'trace_spec_specific_block'    : { 'type' :  'object', 'default' : {}, 'properties' : {
-      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/k', 'data/usedof_k',                                                       'data/m', 'data/usedof_m', 'data/c', 'data/usedof_c' ], 'items' : {
-        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/k', 'data/usedof_k',                                                       'data/m', 'data/usedof_m', 'data/c', 'data/usedof_c' ]
+      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/k',  'data/usedof_k',
+                                                                                   'data/m',  'data/usedof_m', 
+                                                                                   'data/c',  'data/usedof_c'  ], 'items' : {
+        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/k',  'data/usedof_k',
+                                                                                                                                           'data/m',  'data/usedof_m', 
+                                                                                                                                           'data/c',  'data/usedof_c'  ]
       } }
     } },
     # trace spec. specific fields: kernel = function 
     'trace_spec_specific_function' : { 'type' :  'object', 'default' : {}, 'properties' : {
-      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/x', 'data/usedof_x', 'data/r', 'data/usedof_r' ], 'items' : {
-        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/x', 'data/usedof_x', 'data/r', 'data/usedof_r' ]
+      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/x0', 'data/usedof_x0', 
+                                                                                   'data/r0', 'data/usedof_r0' ], 'items' : {
+        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/x0', 'data/usedof_x0', 
+                                                                                                                                           'data/x1', 'data/usedof_x1', 
+                                                                                                                                           'data/x2', 'data/usedof_x2', 
+                                                                                                                                           'data/x3', 'data/usedof_x3', 
+                                                                                                                                           'data/x4', 'data/usedof_x4', 
+                                                                                                                                           'data/x5', 'data/usedof_x5', 
+                                                                                                                                           'data/x6', 'data/usedof_x6', 
+                                                                                                                                           'data/x7', 'data/usedof_x7', 
+                                                                                                                                           'data/r0', 'data/usedof_r0', 
+                                                                                                                                           'data/r1', 'data/usedof_r1', 
+                                                                                                                                           'data/r2', 'data/usedof_r2', 
+                                                                                                                                           'data/r3', 'data/usedof_r3', 
+                                                                                                                                           'data/r4', 'data/usedof_r4', 
+                                                                                                                                           'data/r5', 'data/usedof_r5', 
+                                                                                                                                           'data/r6', 'data/usedof_r6', 
+                                                                                                                                           'data/r7', 'data/usedof_r7' ]
       } }
     } },
     # trace spec. specific fields: kernel = hash     
     'trace_spec_specific_hash'     : { 'type' :  'object', 'default' : {}, 'properties' : {
-      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/m', 'data/usedof_m', 'data/d', 'data/usedof_d' ], 'items' : {
-        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/m', 'data/usedof_m', 'data/d', 'data/usedof_d' ]
+      'content' : { 'type' : 'array', 'default' : [ 'trace/signal', 'crop/signal', 'data/m',  'data/usedof_m', 
+                                                                                   'data/d',  'data/usedof_d'  ], 'items' : {
+        'enum'  : [ 'trace/trigger', 'trace/signal', 'crop/trigger', 'crop/signal', 'perf/cycle', 'perf/duration', 'tvla/lhs', 'tvla/rhs', 'data/m',  'data/usedof_m', 
+                                                                                                                                           'data/d',  'data/usedof_d'  ]
       } }
     } }
   },
@@ -87,18 +117,18 @@ MANIFEST_REQ = {
           'policy_id'   : { 'type' :  'string', 'default' : 'user', 'enum' : [ 'user'         ] },
           'policy_spec' : { 'type' :  'object', 'default' : {}, 'properties' : {
             'user_select' : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'k' : { 'type' : 'string', 'default' :  'all', 'enum' : [ 'all', 'each' ] },
-              'n' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
-              'a' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
-              'm' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
-              'c' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
+              'k'  : { 'type' : 'string', 'default' :  'all', 'enum' : [ 'all', 'each' ] },
+              'n'  : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'a'  : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'm'  : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'c'  : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
             }, 'required' : [] },
             'user_value'  : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'k' : { 'type' : 'string', 'default' : '{$*|k|}'                          },
-              'n' : { 'type' : 'string', 'default' : '{$*|n|}'                          },
-              'a' : { 'type' : 'string', 'default' : '{$*|a|}'                          },
-              'm' : { 'type' : 'string', 'default' : '{$*|m|}'                          },
-              'c' : { 'type' : 'string', 'default' : '{$*|c|}'                          }
+              'k'  : { 'type' : 'string', 'default' : '{$*|k|}'                          },
+              'n'  : { 'type' : 'string', 'default' : '{$*|n|}'                          },
+              'a'  : { 'type' : 'string', 'default' : '{$*|a|}'                          },
+              'm'  : { 'type' : 'string', 'default' : '{$*|m|}'                          },
+              'c'  : { 'type' : 'string', 'default' : '{$*|c|}'                          }
             }, 'required' : [] }
           } }
         } },
@@ -113,14 +143,14 @@ MANIFEST_REQ = {
           'policy_id'   : { 'type' :  'string', 'default' : 'user', 'enum' : [ 'user', 'tvla' ] },
           'policy_spec' : { 'type' :  'object', 'default' : {}, 'properties' : {
             'user_select' : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'k' : { 'type' : 'string', 'default' :  'all', 'enum' : [ 'all', 'each' ] },
-              'm' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
-              'c' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
+              'k'  : { 'type' : 'string', 'default' :  'all', 'enum' : [ 'all', 'each' ] },
+              'm'  : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'c'  : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
             }, 'required' : [] },
             'user_value'  : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'k' : { 'type' : 'string', 'default' : '{$*|k|}'                          },
-              'm' : { 'type' : 'string', 'default' : '{$*|m|}'                          },
-              'c' : { 'type' : 'string', 'default' : '{$*|c|}'                          }
+              'k'  : { 'type' : 'string', 'default' : '{$*|k|}'                          },
+              'm'  : { 'type' : 'string', 'default' : '{$*|m|}'                          },
+              'c'  : { 'type' : 'string', 'default' : '{$*|c|}'                          }
             }, 'required' : [] }
           } }
         } },
@@ -135,10 +165,24 @@ MANIFEST_REQ = {
           'policy_id'   : { 'type' :  'string', 'default' : 'user', 'enum' : [ 'user'         ] },
           'policy_spec' : { 'type' :  'object', 'default' : {}, 'properties' : {
             'user_select' : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'x' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
+              'x0' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'x1' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'x2' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'x3' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'x4' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'x5' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'x6' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] },
+              'x7' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
             }, 'required' : [] },
             'user_value'  : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'x' : { 'type' : 'string', 'default' : '{$*|x|}'                          }
+              'x0' : { 'type' : 'string', 'default' : '{$*|x0|}'                         },
+              'x1' : { 'type' : 'string', 'default' : '{$*|x1|}'                         },
+              'x2' : { 'type' : 'string', 'default' : '{$*|x2|}'                         },
+              'x3' : { 'type' : 'string', 'default' : '{$*|x3|}'                         },
+              'x4' : { 'type' : 'string', 'default' : '{$*|x4|}'                         },
+              'x5' : { 'type' : 'string', 'default' : '{$*|x5|}'                         },
+              'x6' : { 'type' : 'string', 'default' : '{$*|x6|}'                         },
+              'x7' : { 'type' : 'string', 'default' : '{$*|x7|}'                         }
             }, 'required' : [] }
           } }
         } },
@@ -153,10 +197,10 @@ MANIFEST_REQ = {
           'policy_id'   : { 'type' :  'string', 'default' : 'user', 'enum' : [ 'user'         ] },
           'policy_spec' : { 'type' :  'object', 'default' : {}, 'properties' : {
             'user_select' : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'm' : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
+              'm'  : { 'type' : 'string', 'default' : 'each', 'enum' : [ 'all', 'each' ] }
             }, 'required' : [] },
             'user_value'  : { 'type' :  'object', 'default' : {}, 'properties' : {
-              'm' : { 'type' : 'string', 'default' : '{$*|m|}'                          }
+              'm'  : { 'type' : 'string', 'default' : '{$*|m|}'                          }
             }, 'required' : [] }
           } }
         } },
